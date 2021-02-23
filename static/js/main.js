e41d1fdb1b742c42c7cb5b50a3dd82490e4c195c
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-23 15:14:49 +0000

    add side navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,12 @@
-console.log("I work")
+// show side nav when hamburger clicked
+let hamburger = document.querySelector(".hamburger")
+let sideNav = document.querySelector(".side-nav")
+let navClose = document.querySelector(".nav-close")
+
+
+hamburger.addEventListener("click", ()=>{
+    sideNav.classList.add("side-nav-show")
+})
+navClose.addEventListener("click", () => {
+        sideNav.classList.remove("side-nav-show")
+})
