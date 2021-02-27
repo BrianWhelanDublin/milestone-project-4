8efdbc338861754e20c0752f49e9df658c621392
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-27 14:22:46 +0000

    add javascript for new item slider

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,13 @@
 // change top navigation colour on scroll
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
     let topNavigation = document.querySelector(".top-navigation");
-    if(window.scrollY >= 100){
+    if (window.scrollY >= 100) {
         topNavigation.classList.add("top-nav-colored")
-    }
-    else{
+    } else {
         topNavigation.classList.remove("top-nav-colored")
     }
 })
+
 // show side nav when hamburger clicked
 let hamburger = document.querySelector(".hamburger")
 let sideNav = document.querySelector(".side-nav")
@@ -18,18 +18,21 @@ hamburger.addEventListener("click", () => {
     sideMenu.classList.add("side-menu-open")
     sideMenu.classList.remove("side-menu-close")
 })
+
 // show furniture sublist
 let furnitureLink = document.querySelector(".furniture-nav-link")
 let furnitureSubList = document.querySelector(".furniture-sublist")
 furnitureLink.addEventListener("click", () => {
     furnitureSubList.classList.toggle("sublist-show")
 })
+
 // show search sublist
 let searchNavLink = document.querySelector(".search-navlink")
 let searchSubList = document.querySelector(".search-sublist")
 searchNavLink.addEventListener("click", () => {
     searchSubList.classList.toggle("sublist-show")
 })
+
 // close navbar when the close button is clicked
 navClose.addEventListener("click", () => {
     sideMenu.classList.remove("side-menu-open")
@@ -43,4 +46,37 @@ navClose.addEventListener("click", () => {
     setTimeout(() => {
         sideNav.classList.remove("side-nav-show")
     }, 500)
+})
+
+// New-in stock slider
+let rightArrow = document.querySelector("#arrow-right");
+let leftArrow = document.querySelector("#arrow-left");
+let newItemSlider = document.querySelector(".new-item-slider")
+
+let mediaQuery = window.matchMedia('(max-width: 1200px)')
+
+rightArrow.addEventListener("click", () => {
+    if (mediaQuery.matches) {
+        newItemSlider.scrollLeft += 290;
+    } else {
+        newItemSlider.scrollLeft += 420;
+    }
+    leftArrow.classList.add("arrow-active");
+    if (newItemSlider.scrollLeft >= 2500) {
+        rightArrow.classList.remove("arrow-active")
+    }
+})
+leftArrow.addEventListener("click", () => {
+    if (mediaQuery.matches) {
+        newItemSlider.scrollLeft -= 290;
+        if (newItemSlider.scrollLeft <= 290) {
+            leftArrow.classList.remove("arrow-active")
+        }
+    } else {
+        newItemSlider.scrollLeft -= 420;
+        if (newItemSlider.scrollLeft <= 420) {
+            leftArrow.classList.remove("arrow-active")
+        }
+    }
+    rightArrow.classList.add("arrow-active");
 })
