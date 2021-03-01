52759ac53ce7e2967f079023299bfcb1393b67da
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-01 13:52:50 +0000

    set up single item page

--- stock/urls.py
+++ stock/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.all_items, name="all_items"),
+    path("<int:item_id>/", views.single_item, name="single_item"),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 
@@ -12,6 +12,22 @@ def all_items(request):
     context = {
         "items": items
     }
+    return render(request,
+                  template,
+                  context)
+
+
+def single_item(request, item_id):
+    ''' Shows the single item page '''
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    template = "stock/item.html"
+
+    context = {
+        "item": item
+    }
+
     return render(request,
                   template,
                   context)
