10d43228a1a2afd186839f95e935ab5be7f3c64d
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-01 11:04:24 +0000

    custimize the admin display

--- stock/admin.py
+++ stock/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Category, Item
 
 
-# Register Item and Category models.
-admin.site.register(Item)
-admin.site.register(Category)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "name",
+        "category",
+        "price",
+        "image"
+    )
+
+    ordering = (
+        "code",
+        )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "display_name",
+        "name",
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
