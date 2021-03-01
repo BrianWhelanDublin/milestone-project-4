b45743b3dee937aa0e3fa687d478ba1caace2402
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-01 18:08:48 +0000

    add query functionality

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (render, get_object_or_404,
+                              reverse)
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
 
 
@@ -6,12 +9,27 @@ def all_items(request):
     ''' Shows all items page  '''
 
     items = Item.objects.all()
+    search = None
+
+    ''' Handles the search queries from the navigation,
+    using the django Q object to filter the results '''
+    if request.GET:
+        if "query" in request.GET:
+            search = request.GET["query"]
+        if not search:
+            messages.error(request, "No search query entered.")
+
+        search_items = Q(name__icontains=search) \
+            | Q(description__icontains=search)
+        items = items.filter(search_items)
 
     template = "stock/all_items.html"
 
     context = {
-        "items": items
+        "items": items,
+        "search": search,
     }
+
     return render(request,
                   template,
                   context)
