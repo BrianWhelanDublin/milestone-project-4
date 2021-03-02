7b0c7d43ba6465bf7691c15074b71a1b1fab02a5
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-02 11:00:00 +0000

    add pagination to all items

--- stock/views.py
+++ stock/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import (render, get_object_or_404,
                               reverse)
 from django.contrib import messages
 from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import Item
 
 
 def all_items(request):
-    ''' Shows all items page  '''
+    ''' Shows all items page, The view gets all items
+    form the database and then uses Django paginator to paginate them.
+    It then handles the search queries from the navigation,
+    using the django Q object to filter the results.
+    '''
 
     items = Item.objects.all()
     search = None
 
-    ''' Handles the search queries from the navigation,
-    using the django Q object to filter the results '''
-    if request.GET:
-        if "query" in request.GET:
-            search = request.GET["query"]
+    if request.GET and "query" in request.GET:
+        search = request.GET["query"]
         if not search:
             messages.error(request, "No search query entered.")
 
@@ -25,8 +27,12 @@ def all_items(request):
 
     template = "stock/all_items.html"
 
+    paginator = Paginator(items, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "items": items,
+        "page_obj": page_obj,
         "search": search,
     }
 
