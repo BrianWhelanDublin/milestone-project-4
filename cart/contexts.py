941d4bedc8baa8427c722488d2b124cf94ee4472
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-04 10:04:10 +0000

    add delivery logic for smaller items

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from stock.models import Item
+from decimal import Decimal
 
 
 def cart_contents(request):
@@ -23,7 +24,14 @@ def cart_contents(request):
             "item": item,
         })
 
-    home_delivery = settings.STANDARD_HOME_DELIVERY_COST
+    delivery_cost = total * Decimal(settings.DELIVERY_COST_PERCENTAGE/100)
+
+    if delivery_cost < settings.STANDARD_HOME_DELIVERY_COST_MIN:
+        home_delivery = settings.STANDARD_HOME_DELIVERY_COST_MIN
+    elif delivery_cost > settings.STANDARD_HOME_DELIVERY_COST_MAX:
+        home_delivery = settings.STANDARD_HOME_DELIVERY_COST_MAX
+    else:
+        home_delivery = delivery_cost
 
     subtotal = home_delivery + total
 
