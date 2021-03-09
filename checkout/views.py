2d9115da7bdc59c048d5989348f458102d5ae060
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-09 11:00:34 +0000

    install stripe and set up in view

--- checkout/views.py
+++ checkout/views.py
@@ -3,22 +3,44 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 
 def checkout(request):
     ''' a view for the checkout page '''
+
+    # stripe variables
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
 
     cart = request.session.get("cart", {})
     if not cart:
         messages.error(request, "Your cart is currently empty.")
         return redirect(reverse("all_items"))
 
+    # get total for stripe
+    current_cart = cart_contents(request)
+    cart_total = current_cart["subtotal"]
+    stripe_total = round(cart_total * 100)
+
+    # create stripe payment intent
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+
     order_form = OrderForm()
     template = "checkout/checkout.html"
 
     context = {
         "form": order_form,
-        "stripe_public_key": settings.STRIPE_PUBLIC_KEY
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
     }
 
     return render(request,
