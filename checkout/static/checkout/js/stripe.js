aa53ab2b5ed34d8e1a1d55b16e10cf59e8160106
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-09 10:34:33 +0000

    add stripe error code

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,6 +1,8 @@
+// javascript to set up the stripe card element using the stripe documentation.
+
 let stripePublicKey = document.querySelector("#id_stripe_public_key").innerHTML.slice(1, -1);
 
-let clientSecret = document.querySelector("#id_client_secret").innerHTML.slice(1,-1);
+let clientSecret = document.querySelector("#id_client_secret").innerHTML.slice(1, -1);
 
 let stripe = Stripe(stripePublicKey);
 
@@ -14,7 +16,7 @@ let style = {
         fontSize: "14px",
         "::placeholder": {
             color: "rgba(21, 50, 67, .7)"
-            
+
         }
     },
     invalid: {
@@ -23,5 +25,24 @@ let style = {
     }
 };
 
-let card = elements.create("card", {style:style});
-card.mount("#card-element")
+let card = elements.create("card", {
+    style: style
+});
+card.mount("#card-element");
+
+
+// Javascript to handle realtime errors
+
+card.addEventListener("change", (event) => {
+let errorDisplay = document.querySelector("#card-errors");
+if (event.error){
+    markup = `
+   <p class="fs-5 mt-1">
+   <i class="las la-exclamation-circle fs-5" aria-hidden="true"></i>
+   ${event.error.message}</p>
+    `
+    errorDisplay.innerHTML = markup;
+}else{
+    errorDisplay.textContent = "";
+}
+});
