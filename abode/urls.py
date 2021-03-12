d3b450a8f7c219d41a379385f768085f00b07ce1
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-12 15:29:17 +0000

    create the user profile view and set up

--- abode/urls.py
+++ abode/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('items/', include('stock.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('user/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
