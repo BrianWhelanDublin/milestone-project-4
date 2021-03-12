c7eb67f0df1eb2fd7ccde79d582b9e2a95c36b20
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-12 14:52:22 +0000

    create the users app models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_contact_number = models.CharField(max_length=20,
+                                           null=True, blank=True)
+    user_street_address_1 = models.CharField(max_length=80,
+                                             null=True, blank=True)
+    user_street_address_2 = models.CharField(max_length=80,
+                                             null=True, blank=True)
+    user_town_or_city = models.CharField(max_length=50,
+                                         null=True, blank=True)
+    user_county = models.CharField(max_length=50,
+                                   null=True, blank=True)
+    user_eircode = models.CharField(max_length=10,
+                                    null=True, blank=True)
+    user_country = CountryField(blank_label="Country *",
+                                null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_the_userprofile(sender, instance, created, **kwargs):
+    ''' creates the user profile or updates it if there is one already '''
+
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
