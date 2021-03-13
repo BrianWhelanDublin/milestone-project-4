73ad8208a5e064ad605f657d01b001cab696bf1c
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-13 17:19:59 +0000

    start users past orders functionality

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.user_profile, name="user_profile")
+    path("", views.user_profile, name="user_profile"),
+    path("previous_order/<order_number>",
+         views.previous_order, name="previous_order"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,17 +1,47 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def user_profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
     template = "users/user_profile.html"
 
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             "Your profile has been updated successfully.")
+
     form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     context = {
         "form": form,
+        "on_profile_page": True,
+        "orders": orders,
+    }
+
+    return render(request,
+                  template,
+                  context)
+
+
+def previous_order(request):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, "This is a previous order")
+
+    template = "checkout/checkout_success.html"
+    context = {
+        "order": order,
+        "previous_order": True,
     }
 
     return render(request,
