c57b7f9f632f3a14d183d71597602b8aff0c9c18
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-15 10:19:36 +0000

    create Message model

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import NewsletterSubscriber
+from .models import NewsletterSubscriber, Message
 
 
 class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    model = NewsletterSubscriber
     list_display = (
         "email",
     )
 
 
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    readonly_fields = (
+        "subject",
+        "user_email",
+        "message",
+    )
+
+
 admin.site.register(NewsletterSubscriber,
                     NewsletterSubscriberAdmin)
+admin.site.register(Message, MessageAdmin)
--- homepage/models.py
+++ homepage/models.py
@@ -7,3 +7,12 @@ class NewsletterSubscriber(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    user_email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.subject
