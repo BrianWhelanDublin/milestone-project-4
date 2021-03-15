0823faf57918bc448193c665d01ce8c6053f2d71
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-15 10:33:16 +0000

    create message form

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import NewsletterSubscriber
+from .models import NewsletterSubscriber, Message
 
 
 class NewsletterForm(forms.ModelForm):
@@ -15,3 +15,29 @@ class NewsletterForm(forms.ModelForm):
 
         self.fields["email"].widget.attrs["placeholder"] = "Email"
         self.fields["email"].widget.attrs['class'] = "newsletter-form-input"
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+
+    fields = (
+        "subject",
+        "user_email",
+        "message",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            "subject": "Subject",
+            "user_email": "Email",
+            "message": "Message"
+            }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f"{placeholders[field]} *"
+            self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].widget.attrs["class"] = "contact-form-field"
