4f06532865ba32a0fef8ee0b6fb27c9896ccbe12
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-16 11:05:54 +0000

    add if else statements to prevent get requests

--- cart/views.py
+++ cart/views.py
@@ -21,67 +21,77 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     ''' A view to add items to the cart '''
+    if request.method == "POST":
+        item = get_object_or_404(Item, pk=item_id)
+        quantity = int(request.POST.get("quantity"))
+        redirect_url = request.POST.get("redirect_url")
 
-    item = get_object_or_404(Item, pk=item_id)
-    quantity = int(request.POST.get("quantity"))
-    redirect_url = request.POST.get("redirect_url")
+        cart = request.session.get("cart", {})
 
-    cart = request.session.get("cart", {})
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+            messages.success(request,
+                            f"{item.name}'s \
+                                quantity has been updated \
+                                    to {cart[item_id]}")
+        else:
+            cart[item_id] = quantity
+            messages.success(request,
+                            f"{item.name} \
+                                has been added to your cart.")
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request,
-                         f"{item.name}'s \
-                              quantity has been updated \
-                                  to {cart[item_id]}")
+        request.session["cart"] = cart
+
+        return redirect(redirect_url)
     else:
-        cart[item_id] = quantity
-        messages.success(request,
-                         f"{item.name} \
-                             has been added to your cart.")
-
-    request.session["cart"] = cart
-
-    return redirect(redirect_url)
+        messages.error(request, "Error you cannot do this")
+        return redirect(reverse("home_page"))
 
 
 def update_cart(request, item_id):
     ''' View to update the cart when the quantity has changed '''
+    if request.method == "POST":
+        item = get_object_or_404(Item, pk=item_id)
+        quantity = int(request.POST.get("item_quantity"))
 
-    item = get_object_or_404(Item, pk=item_id)
-    quantity = int(request.POST.get("item_quantity"))
+        cart = request.session.get("cart", {})
 
-    cart = request.session.get("cart", {})
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request,
+                            f"{item.name}'s \
+                                quantity has been updated to {cart[item_id]}")
+        else:
+            cart.pop(item_id)
+            messages.success(request,
+                            f"{item.name} has been removed from your cart.")
 
-    if quantity > 0:
-        cart[item_id] = quantity
-        messages.success(request,
-                         f"{item.name}'s \
-                             quantity has been updated to {cart[item_id]}")
+        request.session["cart"] = cart
+
+        return redirect(reverse("view_cart"))
     else:
-        cart.pop(item_id)
-        messages.success(request,
-                         f"{item.name} has been removed from your cart.")
-
-    request.session["cart"] = cart
-
-    return redirect(reverse("view_cart"))
+        messages.error(request, "Error you cannot do this")
+        return redirect(reverse("home_page"))
 
 
 def remove_from_cart(request, item_id):
     ''' View to remove items from the cart '''
-    try:
-        item = get_object_or_404(Item, pk=item_id)
+    if request.method == "POST":
+        try:
+            item = get_object_or_404(Item, pk=item_id)
 
-        cart = request.session.get("cart", {})
+            cart = request.session.get("cart", {})
 
-        cart.pop(item_id)
-        messages.success(request,
-                         f"{item.name} has been removed from your cart.")
+            cart.pop(item_id)
+            messages.success(request,
+                            f"{item.name} has been removed from your cart.")
 
-        request.session["cart"] = cart
-        return HttpResponse(status=200)
+            request.session["cart"] = cart
+            return HttpResponse(status=200)
 
-    except Exception as error:
-        messages.error(request, f"Error removing item {error}")
-        return HttpResponse(status=500)
+        except Exception as error:
+            messages.error(request, f"Error removing item {error}")
+            return HttpResponse(status=500)
+    else:
+        messages.error(request, "Error you cannot do this")
+        return redirect(reverse("home_page"))
