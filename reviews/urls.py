c1facdf54fae1ca9b883b16203f14b678c2788ab
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-17 16:40:51 +0000

    add the edit review function

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.our_reviews, name="our_reviews")
+    path("", views.our_reviews, name="our_reviews"),
+    path("edit/<review_id>/", views.edit_review, name="edit_review"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .forms import ReviewForm
 from .models import Review
 from django.core.paginator import Paginator
@@ -20,6 +21,42 @@ def our_reviews(request):
     page_obj = paginator.get_page(page_number)
 
     form = ReviewForm()
+    template = "reviews/our_reviews.html"
+    context = {
+        "form": form,
+        "page_obj": page_obj,
+    }
+
+    return render(request,
+                  template,
+                  context)
+
+
+def edit_review(request, review_id):
+    ''' View to edit a users review '''
+    
+    review = get_object_or_404(Review, pk=review_id)
+    if review.reviewer != request.user:
+        messages.error(request,
+                       "You do not have permission to do this.")
+        return redirect(reverse("our_reviews"))
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             "Review has been updated.")
+            return redirect(reverse("our_reviews"))
+        else:
+            messages.error(request,
+                           "Failed to update review. Please try again later.")
+    else:
+        form = ReviewForm(instance=review)
+
+    reviews = Review.objects.all().order_by("-id")
+    paginator = Paginator(reviews, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = "reviews/our_reviews.html"
     context = {
         "form": form,
