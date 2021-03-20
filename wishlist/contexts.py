e91cdd2825e35a69ff2b51286b024a3966bd28b4
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-20 09:28:03 +0000

    move wishlist context into a context processor

---
+++ wishlist/contexts.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from stock.models import Item
+
+
+def wishlist_items(request):
+
+    wishlist_items = []
+    wishlist_count = 0
+    wishlist = request.session.get("wishlist", {})
+
+    for item_id in wishlist:
+        item = get_object_or_404(Item, pk=item_id)
+        wishlist_count += 1
+        wishlist_items.append({"item": item})
+
+    context = {
+        "wishlist_items": wishlist_items,
+        "wishlist_count": wishlist_count,
+    }
+
+    return context
