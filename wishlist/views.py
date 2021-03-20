8b0001a70d6488ef053a049bc741ec38e97c4db7
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-20 09:29:05 +0000

    move wishlist context and edit views

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,35 +6,22 @@ from django.contrib import messages
 def view_wishlist(request):
     ''' a view to show the users wishlist '''
 
-    wishlist_items = []
-    wishlist = request.session.get("wishlist", {})
-
-    for item_id in wishlist:
-        item = get_object_or_404(Item, pk=item_id)
-        wishlist_items.append({"item": item})
-
     template = "wishlist/wishlist.html"
-    print(wishlist_items)
-    context = {
-        "wishlist": True,
-        "wishlist_items": wishlist_items,
-    }
-
     return render(request,
-                  template,
-                  context)
+                  template)
 
 
 def add_to_wishlist(request, item_id):
-    # item = get_object_or_404(Item, pk=item_id)
+    if request.method == "POST":
+        wishlist = request.session.get("wishlist", {})
 
-    wishlist = request.session.get("wishlist", {})
-    redirect_url = request.POST.get("redirect_url")
-
-    wishlist[item_id] = item_id
-    request.session["wishlist"] = wishlist
-    item = get_object_or_404(Item, pk=item_id)
-    # return redirect(redirect_url)
-    messages.success(request,
-                     f"{item.name} has been added to your wishlist.")
-    return HttpResponse(status=200)
+        wishlist[item_id] = item_id
+        request.session["wishlist"] = wishlist
+        item = get_object_or_404(Item, pk=item_id)
+        messages.success(request,
+                        f"{item.name} has been added to your wishlist.")
+        return HttpResponse(status=200)
+    else:
+        messages.error(request,
+                       "You do not have permission to do this.")
+        return redirect(reverse("all_items"))
