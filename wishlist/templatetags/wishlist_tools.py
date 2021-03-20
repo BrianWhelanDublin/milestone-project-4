3a8ac7d6bda4d0e76cb01d9f4ba9ddc19bc9bd8e
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-20 16:58:30 +0000

    add like button

--- wishlist/templatetags/wishlist_tools.py
+++ wishlist/templatetags/wishlist_tools.py
@@ -7,7 +7,6 @@ register = template.Library()
 @register.filter(name='ifinlist')
 def ifinlist(value, list):
     key = "item_id"
-    print(value)
     for item in list:
         if item[key] == str(value):
             return True
