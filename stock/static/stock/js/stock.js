327c8890d7294f211317f80f75cea0f2955253fe
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-20 17:24:55 +0000

    add the remove from wishlist button and function

--- stock/static/stock/js/stock.js
+++ stock/static/stock/js/stock.js
@@ -27,8 +27,29 @@ let wishlistButton = document.querySelectorAll(".wishlist-button")
         button.addEventListener("click", (event) => {
             event.preventDefault()
             let itemId = button.getAttribute("id").split("wish_")[1];
-            let url = `/wishlist/add/${itemId}/`
             console.log(itemId)
+            let url = `/wishlist/add/${itemId}/`
+            console.log(url)
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "X-CSRFToken": csrfToken
+                },
+            }).then(() => {
+                location.reload();
+            }).catch((error) => {
+                alert("Opps something has gone wrong.");
+            });
+        })
+    })
+
+let wishlistLikedButton = document.querySelectorAll(".wishlist-button-liked")
+    wishlistLikedButton.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault()
+            let itemId = button.getAttribute("id").split("liked_")[1];
+            console.log(itemId)
+            let url = `/wishlist/remove/${itemId}/`
             fetch(url, {
                 method: "POST",
                 headers: {
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("add/<item_id>/",
          views.add_to_wishlist,
          name="add_to_wishlist"),
+    path("remove/<item_id>/",
+         views.remove_from_wishlist,
+         name="remove_from_wishlist"),
 ]
