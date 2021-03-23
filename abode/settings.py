37487f8846b1ef5ff0c263a6f1b9d1643d93b9a1
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-23 12:04:27 +0000

    set up to deploy to heroku

--- abode/settings.py
+++ abode/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["abode-milestone-4.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -114,12 +114,18 @@ WSGI_APPLICATION = 'abode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# use postgress on deployment otherwise the default database
+if "DATABSE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
