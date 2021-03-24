3264e6e0ef273b66090c94961523253bef8b6514
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-24 11:39:38 +0000

    edit email host

--- abode/settings.py
+++ abode/settings.py
@@ -251,7 +251,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.googlemail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_ADDRESS')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_ADDRESS')
