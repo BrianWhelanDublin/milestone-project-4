9f8e7b97242cc5a0f72ddded68f7a0cf634a56e6
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-27 17:58:32 +0000

    set up code to test blog models

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def view_blog(request):
+    posts = Post.objects.all()
     template = "blog/view_blog.html"
+    context = {
+        "posts": posts
+    }
 
     return render(request,
-                  template)
+                  template,
+                  context)
