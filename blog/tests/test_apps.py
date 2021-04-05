0b87bb53d47fbfe0f33393b90fe4f70ab2774535
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-04-05 13:55:58 +0000

    add the test apps in the blog app

---
+++ blog/tests/test_apps.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from blog import apps
+
+
+class BlogAppTests(TestCase):
+
+    def test_apps_config(self):
+        """ test blog app configured correctly"""
+        self.assertEqual(apps.BlogConfig.name, 'blog')
