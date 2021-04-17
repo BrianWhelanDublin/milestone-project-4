a8c00eac43df14a81914fcad2671bcd6e32bbe61
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-04-17 16:13:16 +0000

    move cart javascript to its own static file in the cart app

---
+++ cart/static/cart/js/cart.js
@@ -0,0 +1,30 @@
+
+    // code to update quantity of item in cart
+    let updateLink = document.querySelectorAll(".update-quantity-link");
+
+    updateLink.forEach((link) => {
+        link.addEventListener("click", () => {
+            let form = link.previousElementSibling
+            form.submit()
+        })
+    })
+    // code to remove items from the cart
+    let removeLink = document.querySelectorAll(".remove-cart-item");
+
+    removeLink.forEach((link) => {
+        link.addEventListener("click", () => {
+            let itemId = link.getAttribute("id").split("remove_")[1];
+            let url = `/cart/remove/${itemId}/`
+
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "X-CSRFToken": csrfToken
+                },
+            }).then(() => {
+                location.reload();
+            }).catch((error) => {
+                alert("Opps something has gone wrong.");
+            });
+        });
+    });
