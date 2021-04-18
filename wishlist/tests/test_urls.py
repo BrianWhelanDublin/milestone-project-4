5f8b471e008a91d40d5f8d1bfe312a5429362c6b
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-04-18 14:45:51 +0000

    add the urls tests to the wishlist app

---
+++ wishlist/tests/test_urls.py
@@ -0,0 +1,25 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from wishlist.views import (view_wishlist, remove_from_wishlist,
+                            add_to_wishlist, delete_wishlist)
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_view_wishlist_url(self):
+        url = reverse("view_wishlist")
+        self.assertEquals(resolve(url).func, view_wishlist)
+
+    def test_remove_from_wishlist_url(self):
+        url = reverse("remove_from_wishlist", args=["1"])
+        self.assertEquals(resolve(url).func, remove_from_wishlist)
+
+    def test_add_to_wishlist_url(self):
+        url = reverse("add_to_wishlist", args=["1"])
+        self.assertEquals(resolve(url).func, add_to_wishlist)
+
+    def test_delete_wishlist_url(self):
+        url = reverse("delete_wishlist")
+        self.assertEquals(resolve(url).func, delete_wishlist)
+
+
