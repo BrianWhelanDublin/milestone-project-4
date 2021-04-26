cefaf1b7bc7dacd4667503ee7888e3cea28f388b
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-04-26 16:54:30 +0000

    add error page templates into the main template file

--- abode/settings.py
+++ abode/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["abode-milestone-4.herokuapp.com", "localhost"]
 
