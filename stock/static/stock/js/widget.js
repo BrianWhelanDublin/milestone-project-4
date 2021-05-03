b23bc2cd343714c85c6b7110b5793f807a77c8ad
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-05-03 11:25:32 +0000

    edit the file input widget in the stock app to fix the html errors with double id's

--- stock/static/stock/js/widget.js
+++ stock/static/stock/js/widget.js
@@ -1,5 +1,5 @@
 // places the image name underneat the add image button
-let newImageInput = document.querySelector("#new-image-input");
+let newImageInput = document.querySelector(".new-image-input");
 newImageInput.addEventListener("change", ()=>{
    let file = newImageInput.files[0];
    document.querySelector("#image-name").innerHTML = `
