1df4cfe12e0b7d43bd8928b7215b6cefcc621d0f
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-05-23 13:13:26 +0000

    edit functionality of new item slider on the homepage

--- homepage/static/homepage/js/homepage.js
+++ homepage/static/homepage/js/homepage.js
@@ -1,35 +1,3 @@
-// New-in stock slider
-
-let rightArrow = document.querySelector("#arrow-right");
-let leftArrow = document.querySelector("#arrow-left");
-let newItemSlider = document.querySelector(".new-item-slider");
-let slide = document.querySelector(".item-container");
-let slidesAmount = newItemSlider.getElementsByTagName("li").length;
-let slideWidth = slide.offsetWidth;
-let sliderWidth = newItemSlider.clientWidth;
-let sliderMaxLength = slidesAmount * slideWidth;
-let lastSlide = sliderMaxLength - sliderWidth - (slideWidth/2);
-
-rightArrow.addEventListener("click", () => {
-    newItemSlider.scrollLeft += slideWidth;
-    leftArrow.classList.add("arrow-active");
-    console.log(newItemSlider.scrollLeft)
-    console.log(lastSlide)
-    if (newItemSlider.scrollLeft >= lastSlide) {
-        rightArrow.classList.remove("arrow-active");
-    }
-});
-
-leftArrow.addEventListener("click", () => {
-    newItemSlider.scrollLeft -= slideWidth;
-    console.log(newItemSlider.scrollLeft)
-    console.log(slideWidth)
-    if (newItemSlider.scrollLeft < slideWidth) {
-        leftArrow.classList.remove("arrow-active");
-    }
-    rightArrow.classList.add("arrow-active");
-});
-
 // change top navigation colour on scroll
 window.addEventListener("scroll", () => {
     let topNavigation = document.querySelector(".top-navigation");
@@ -39,3 +7,61 @@ window.addEventListener("scroll", () => {
         topNavigation.classList.remove("top-nav-colored");
     }
 });
+
+
+const container = document.querySelector(".new-item-slider");
+const allItems = document.querySelectorAll(".item-container");
+const firstItem = document.querySelector(".item-container");
+const rightArrow = document.querySelector("#arrow-right")
+const leftArrow = document.querySelector("#arrow-left")
+// const containerWidth = container.offsetWidth;
+const itemWidth = firstItem.clientWidth;
+const margin = 20
+let items = 0
+// let totalItems = 0;
+let jumpSlideWidth = 0;
+let slider = document.querySelector(".new-item-slider-container")
+let sliderWidth = slider.offsetWidth
+
+breakPoints = [
+    { breakPoint: { width: 0, item: 1 } }, 
+    { breakPoint: { width: 760, item: 2 } },
+    { breakPoint: { width: 992, item:3 } },
+    { breakPoint: { width: 1400, item: 4 } }, 
+];
+
+const load = () => {
+    breakPoints.forEach(el => {
+        if (window.innerWidth > el.breakPoint.width) {
+            items = el.breakPoint.item;
+        }
+    });
+    setItemsMargins();
+};
+
+const setItemsMargins = () => {
+    totalItemsWidth = 0
+    allItems.forEach(el => {
+       el.style.margin = (margin / 2) + "px";
+        totalItemsWidth += (margin + itemWidth)
+    })
+}
+
+rightArrow.addEventListener("click", () => {
+    let totalWidth = sliderWidth * (allItems.length / items)
+    if (rightArrow.classList.contains("clickable")) jumpSlideWidth = jumpSlideWidth + (itemWidth + margin);
+    if (jumpSlideWidth > 0) leftArrow.classList.add("clickable")
+    if (jumpSlideWidth < (totalWidth)) container.style.marginLeft = -jumpSlideWidth + "px";
+    if ((jumpSlideWidth >= (totalWidth - sliderWidth)) && (jumpSlideWidth < (totalWidth))) rightArrow.classList.remove("clickable")
+})
+
+leftArrow.addEventListener("click", () => {
+    if (leftArrow.classList.contains("clickable")) jumpSlideWidth = jumpSlideWidth - (itemWidth + margin);
+    if (jumpSlideWidth >= 0) {
+        container.style.marginLeft = -jumpSlideWidth + "px";
+        rightArrow.classList.add("clickable")
+    }
+    if (jumpSlideWidth == 0) leftArrow.classList.remove("clickable")
+})
+
+load();
