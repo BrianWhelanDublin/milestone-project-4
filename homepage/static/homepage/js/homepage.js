4c0a984656927494bcf8156ce9a48eb2b936f150
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-05-23 13:51:39 +0000

    edit javascript in homepage to validate it

--- homepage/static/homepage/js/homepage.js
+++ homepage/static/homepage/js/homepage.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */ 
+
 // change top navigation colour on scroll
 window.addEventListener("scroll", () => {
     let topNavigation = document.querySelector(".top-navigation");
@@ -8,22 +10,20 @@ window.addEventListener("scroll", () => {
     }
 });
 
-
+// new items slider code inspired by The WebShala on youtube details in readme.
 const container = document.querySelector(".new-item-slider");
 const allItems = document.querySelectorAll(".item-container");
 const firstItem = document.querySelector(".item-container");
-const rightArrow = document.querySelector("#arrow-right")
-const leftArrow = document.querySelector("#arrow-left")
-// const containerWidth = container.offsetWidth;
+const rightArrow = document.querySelector("#arrow-right");
+const leftArrow = document.querySelector("#arrow-left");
 const itemWidth = firstItem.clientWidth;
-const margin = 20
-let items = 0
-// let totalItems = 0;
+const margin = 20;
+let items = 0;
 let jumpSlideWidth = 0;
-let slider = document.querySelector(".new-item-slider-container")
-let sliderWidth = slider.offsetWidth
+let slider = document.querySelector(".new-item-slider-container");
+let sliderWidth = slider.offsetWidth;
 
-breakPoints = [
+let breakPoints = [
     { breakPoint: { width: 0, item: 1 } }, 
     { breakPoint: { width: 760, item: 2 } },
     { breakPoint: { width: 992, item:3 } },
@@ -40,28 +40,28 @@ const load = () => {
 };
 
 const setItemsMargins = () => {
-    totalItemsWidth = 0
+    let totalItemsWidth = 0;
     allItems.forEach(el => {
        el.style.margin = (margin / 2) + "px";
-        totalItemsWidth += (margin + itemWidth)
-    })
-}
+        totalItemsWidth += (margin + itemWidth);
+    });
+};
 
 rightArrow.addEventListener("click", () => {
-    let totalWidth = sliderWidth * (allItems.length / items)
+    let totalWidth = sliderWidth * (allItems.length / items);
     if (rightArrow.classList.contains("clickable")) jumpSlideWidth = jumpSlideWidth + (itemWidth + margin);
-    if (jumpSlideWidth > 0) leftArrow.classList.add("clickable")
+    if (jumpSlideWidth > 0) leftArrow.classList.add("clickable");
     if (jumpSlideWidth < (totalWidth)) container.style.marginLeft = -jumpSlideWidth + "px";
-    if ((jumpSlideWidth >= (totalWidth - sliderWidth)) && (jumpSlideWidth < (totalWidth))) rightArrow.classList.remove("clickable")
-})
+    if ((jumpSlideWidth >= (totalWidth - sliderWidth)) && (jumpSlideWidth < (totalWidth))) rightArrow.classList.remove("clickable");
+});
 
 leftArrow.addEventListener("click", () => {
     if (leftArrow.classList.contains("clickable")) jumpSlideWidth = jumpSlideWidth - (itemWidth + margin);
     if (jumpSlideWidth >= 0) {
         container.style.marginLeft = -jumpSlideWidth + "px";
-        rightArrow.classList.add("clickable")
+        rightArrow.classList.add("clickable");
     }
-    if (jumpSlideWidth == 0) leftArrow.classList.remove("clickable")
-})
+    if (jumpSlideWidth == 0) leftArrow.classList.remove("clickable");
+});
 
 load();
