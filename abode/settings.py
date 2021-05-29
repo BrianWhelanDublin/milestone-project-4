fc47b1263214829df5ca563a12c85f58096d88fc
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-05-29 13:38:34 +0000

    add debug settings to setings.py for in development and production

--- abode/settings.py
+++ abode/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "IN_DEVELOPMENT" in os.environ
+if "IN_DEVELOPMENT" in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ["abode-milestone-4.herokuapp.com", "localhost"]
 
